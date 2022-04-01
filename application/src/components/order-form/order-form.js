f7685364dcf885a1235d0409866526cd934b09f6
Logan Hernandez <logankhernandez@gmail.com>
2022-03-31 22:20:00 -0700

    fix(order-form): resolve #1

--- application/src/components/order-form/order-form.js
+++ application/src/components/order-form/order-form.js
@@ -10,8 +10,8 @@ export default function OrderForm(props) {
     const [orderItem, setOrderItem] = useState("");
     const [quantity, setQuantity] = useState("1");
 
-    const menuItemChosen = (event) => setOrderItem(event.value);
-    const menuQuantityChosen = (event) => setQuantity(event.value);
+    const menuItemChosen = (event) => setOrderItem(event.target.value);
+    const menuQuantityChosen = (event) => setQuantity(event.target.value);
 
     const auth = useSelector((state) => state.auth);
 
@@ -40,7 +40,7 @@ export default function OrderForm(props) {
                     <label className="form-label">I'd like to order...</label><br />
                     <select 
                         value={orderItem} 
-                        onChange={(event) => menuItemChosen(event)}
+                        onChange={(event) => {menuItemChosen(event)}}
                         className="menu-select"
                     >
                         <option value="" defaultValue disabled hidden>Lunch menu</option>
